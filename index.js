677d780174069ee4d59cf8feab1ab334b46ff732
Eliot Cleveland <eclevela@richmond.edu>
2022-07-22 22:57:42 -0400

    initial questions aded to questions array

--- index.js
+++ index.js
@@ -5,23 +5,70 @@ const inquirer = require('inquirer');
 const questions = [{
     type: 'input',
     name: 'name',
-    message: 'What is your name? (Required)',
+    message: 'What is the name of your project? (Required)',
     validate: nameInput => {
       if (nameInput) {
         return true;
-      } else { 
-        console.log('Please enter your name!');
+      } else {
+        console.log('You need to enter a project name!');
         return false;
       }
     }
-  }];
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of the project. (Required) \n Consider detailing your motivation, what problem it solves, what tools you used, and what you learned. \n',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your Github username (Required)',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('You need to enter a username!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your Github username (Required)',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('You need to enter an email address');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'sections',
+    message: 'What sections would you like to include? (Check all that apply) \n',
+    choices: ['Link to Deployed Site','Installation Instructions', 'Usage Information', 'Contribution Guidelines', 'Test Instructions', 'Collaboration Credits']
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init () {
-    return inquirer.prompt(questions);
+    return inquirer.prompt(questions).then(answers => console.log(answers));
+
 }
 
 // Function call to initialize app
