80e62a841adfc5fcca9fc0eb5d59872790f18bba
Eliot Cleveland <eclevela@richmond.edu>
2022-07-23 23:29:09 -0400

    returns section question array

--- index.js
+++ index.js
@@ -1,82 +1,109 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: 'input',
-    name: 'name',
-    message: 'What is the name of your project? (Required)',
-    validate: nameInput => {
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of your project? (Required)",
+    validate: (nameInput) => {
       if (nameInput) {
         return true;
       } else {
-        console.log('You need to enter a project name!');
+        console.log("You need to enter a project name!");
         return false;
       }
-    }
+    },
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of the project. (Required) \n Consider detailing your motivation, what problem it solves, what tools you used, and what you learned. \n',
-    validate: descriptionInput => {
+    type: "input",
+    name: "description",
+    message:
+      "Provide a description of the project. (Required) \n Consider detailing your motivation, what problem it solves, what tools you used, and what you learned. \n",
+    validate: (descriptionInput) => {
       if (descriptionInput) {
         return true;
       } else {
-        console.log('You need to enter a project description!');
+        console.log("You need to enter a project description!");
         return false;
       }
-    }
+    },
   },
   {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your Github username (Required)',
-    validate: githubInput => {
+    type: "input",
+    name: "github",
+    message: "Enter your Github username (Required)",
+    validate: (githubInput) => {
       if (githubInput) {
         return true;
       } else {
-        console.log('You need to enter a username!');
+        console.log("You need to enter a username!");
         return false;
       }
-    }
+    },
   },
   {
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address (Required)',
-    validate: emailInput => {
+    type: "input",
+    name: "email",
+    message: "Enter your email address (Required)",
+    validate: (emailInput) => {
       if (emailInput) {
         return true;
       } else {
-        console.log('You need to enter an email address');
+        console.log("You need to enter an email address");
         return false;
       }
-    }
+    },
   },
   {
-    type: 'checkbox',
-    name: 'sections',
-    message: 'What sections would you like to include? (Check all that apply) \n',
-    choices: ['Link to Deployed Site','Installation Instructions', 'Usage Information', 'Contribution Guidelines', 'Test Instructions', 'Collaboration Credits']
-  }
+    type: "checkbox",
+    name: "sections",
+    message:
+      "What sections would you like to include? (Check all that apply) \n",
+    choices: [
+      "Link to Deployed Site",
+      "Installation Instructions",
+      "Usage Information",
+      "Contribution Guidelines",
+      "Test Instructions",
+      "Collaboration Credits",
+    ],
+  },
 ];
 
-const promptSections = (sections) => console.log(sections);
+const sectionQuestions = (sectionSubjects) => {
+    let sectionQuestionsArr = []; 
+  for (i = 0; i < sectionSubjects.length; i++) {
+    let sectionQuestionsObj = {
+      type: "input",
+      name: sectionSubjects[i],
+      message: "Add the " + sectionSubjects[i] + " below: \n",
+      validate: (input) => {
+        return input ? true : false;
+      },
+    };
+    sectionQuestionsArr.push(sectionQuestionsObj);
+  }
+  return sectionQuestionsArr;
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init () {
-    return inquirer.prompt(questions)
-    // .then(promptSections)
-    .then(answers => { console.log(answers);
-        const {sections, name, description, ...contact } = answers;
-        promptSections(sections);
-        console.log(name, description, contact);
-    })
-
-
+function init() {
+  return (
+    inquirer
+      .prompt(questions)
+      // .then(promptSections)
+      .then((answers) => {
+        let sectionSubjects = answers.sections;
+        console.log(sectionQuestions(sectionSubjects));
+        console.log(answers);
+        // console.log(name, description, contact);
+      })
+  );
+  // .then(data => console.log(data));
 }
 
 // Function call to initialize app
