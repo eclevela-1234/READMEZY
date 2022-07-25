f6b42773097abef3ebf7d1c16657cca76570fbdc
Eliot Cleveland <eclevela@richmond.edu>
2022-07-24 20:52:54 -0400

    all data passes to promptmain function. it took a while but by golly i figured it out!

--- index.js
+++ index.js
@@ -1,118 +1,108 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-
+const generateREADME = "./utils/generateMarkdown.js";
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the name of your project? (Required)",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("You need to enter a project name!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "description",
-    message:
-      "Provide a description of the project. (Required) \n Consider detailing your motivation, what problem it solves, what tools you used, and what you learned. \n",
-    validate: (descriptionInput) => {
-      if (descriptionInput) {
-        return true;
-      } else {
-        console.log("You need to enter a project description!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Enter your Github username (Required)",
-    validate: (githubInput) => {
-      if (githubInput) {
-        return true;
-      } else {
-        console.log("You need to enter a username!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your email address (Required)",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("You need to enter an email address");
-        return false;
-      }
-    },
-  },
-  {
-    type: "checkbox",
-    name: "sections",
-    message:
-      "What sections would you like to include? (Check all that apply) \n",
-    choices: [
-      "Link to Deployed Site",
-      "Installation Instructions",
-      "Usage Information",
-      "Contribution Guidelines",
-      "Test Instructions",
-      "Collaboration Credits",
-    ],
-  },
-];
-
-const sectionQuestions = (sectionSubjects) => {
-  let sectionQuestionsArr = [];
-  for (i = 0; i < sectionSubjects.length; i++) {
-    let sectionQuestionsObj = {
-      type: "input",
-      name: sectionSubjects[i],
-      message: "Add the " + sectionSubjects[i] + " below: \n",
-      validate: input => {
-        return input ? true : console.log();
-      },
-    };
-    sectionQuestionsArr.push(sectionQuestionsObj);
-  }
-  return sectionQuestionsArr;
-};
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
+const promptMain = () => {
   console.log(`
     ===================
     Welcome to READMEZY
     ===================
     `);
-  return (
-    inquirer
-      .prompt(questions)
-      // .then(promptSections)
-      .then((answers) => {
-        // run page build for secions here
-        let sectionSubjects = answers.sections;
-        inquirer
-          .prompt(sectionQuestions(sectionSubjects))
-          .then((data) => console.log(data));
-
-        // console.log(name, description, contact);
-      })
-  );
-  // .then(data => console.log(data));
-}
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You need to enter a project name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Provide a description of the project. (Required) \n Consider detailing your motivation, what problem it solves, what tools you used, and what you learned. \n",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("You need to enter a project description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your Github username (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("You need to enter a username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address (Required)",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("You need to enter an email address");
+          return false;
+        }
+      },
+    },
+    {
+      type: "checkbox",
+      name: "sections",
+      message:
+        "What sections would you like to include? (Check all that apply) \n",
+      choices: [
+        "Link to Deployed Site",
+        "Installation Instructions",
+        "Usage Information",
+        "Contribution Guidelines",
+        "Test Instructions",
+        "Collaboration Credits",
+      ],
+    },
+  ]);
+};
+const promptSectionContent = (READMEdata) => {
+  READMEdata.sectionContent = [];
+  let sectionArr = [];
+  for (let i = 0; i < READMEdata.sections.length; i++) {
+    let sectionObj = {
+      type: "input",
+      name: READMEdata.sections[i],
+      message: "Add the " + READMEdata.sections[i] + " below: \n",
+      validate: (input) => {
+        return input ? true : console.log("Please enter the info!");
+      },
+    };
+    sectionArr.push(sectionObj);
+  }
+  return inquirer.prompt(sectionArr).then((sectionContentData) => {
+    READMEdata.sectionContent.push(sectionContentData);
+    return READMEdata;
+  });
+};
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
 // Function call to initialize app
-init();
+promptMain()
+  .then(promptSectionContent)
+  .then((READMEdata) => {
+    console.log(READMEdata);
+  });
